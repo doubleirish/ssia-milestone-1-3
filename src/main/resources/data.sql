15d1ec656b44284222877a544887d7a277f6f200
Conor Redmond <credmond@incomm.com>
2020-09-13 22:34:12 -0700

    add more users for milestone 2 testing

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,11 @@
 insert into USER (ID, USERNAME, PASSWORD) values (1, 'john' ,'{bcrypt}$2a$10$iMMb7iGNjDAlqkwlR4TJHuhwtMGq.sMGL5v3TEiCt53vIiGke0cpa');
 insert into USER (ID, USERNAME, PASSWORD) values (2, 'admin' ,'{noop}secret2');
+insert into USER (ID, USERNAME, PASSWORD) values (3, 'bob' ,'{noop}password');
+insert into USER (ID, USERNAME, PASSWORD) values (4, 'alice' ,'{noop}swordfish');
 INSERT INTO AUTHORITY ( USER_ID ,AUTHORITY ) values (1, 'ROLE_USER');
 INSERT INTO AUTHORITY ( USER_ID ,AUTHORITY ) values (2, 'ROLE_ADMIN');
-
+INSERT INTO AUTHORITY ( USER_ID ,AUTHORITY ) values (3, 'ROLE_USER');
+INSERT INTO AUTHORITY ( USER_ID ,AUTHORITY ) values (4, 'ROLE_USER');
 insert into CLIENT (ID, NAME, SECRET, REDIRECT_URI, SCOPE)
 values (1, 'client','{bcrypt}$2a$10$CVLUeCYqZQpLRm0PpaXXTuvskBujQelGhmxoCXXU0RylBrTQOiqQW' ,'http://localhost:8181/', 'read');
 
